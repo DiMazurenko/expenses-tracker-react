ae0856a68ae24d3911bfff8a72e32b805ddcdcad
Dmitriy <dimazurenko@gmail.com>
2022-12-12 19:09:25 +0300

    add listener in form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,13 @@
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const titleChangeHandler = () => {};
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
