78f56af38264bf9eb19d3f937d69d6621a5d148a
Dmitriy <dimazurenko@gmail.com>
2022-12-14 12:26:05 +0300

    update ExpenseForm state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,22 +2,28 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: '',
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (e) => {
-    setUserInput({ ...userInput, enteredTitle: e.target.value });
+    setEnteredTitle(e.target.value);
   };
 
   const amountChangeHandler = (e) => {
-    setUserInput({ ...userInput, enteredAmount: e.target.value });
+    // setUserInput({ ...userInput, enteredAmount: e.target.value });
+    setEnteredAmount(e.target.value);
   };
 
   const dateChangeHandler = (e) => {
-    setUserInput({ ...userInput, enteredDate: e.target.value });
+    // setUserInput({ ...userInput, enteredDate: e.target.value });
+    setEnteredDate(e.target.value);
   };
   return (
     <form>
