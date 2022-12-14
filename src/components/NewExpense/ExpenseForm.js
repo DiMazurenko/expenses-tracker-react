c6857f9ee91d5025d5587013253bec624498c730
Dmitriy <dimazurenko@gmail.com>
2022-12-14 12:35:10 +0300

    add form listener ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -25,8 +25,21 @@ const ExpenseForm = () => {
     // setUserInput({ ...userInput, enteredDate: e.target.value });
     setEnteredDate(e.target.value);
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseDate);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
